2aba02a30d3245b8b41605db7bff1bb46d309621
Iosif Pravets <i@pravets.ru>
2025-04-12 13:55:23 +0300

    fixed encoders for rolio46

--- include/zmk-helpers/key-labels/rolio46.h
+++ include/zmk-helpers/key-labels/rolio46.h
@@ -1,11 +1,11 @@
 /*                                      42 KEY MATRIX / LAYOUT MAPPING
 
-  ╭────────────────────────┬────────────────────────╮ ╭─────────────────────────┬─────────────────────────╮
-  │  0   1   2   3   4   5 │  6   7   8   9  10  11 │ │ LT5 LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 RT5 │
-  │ 12  13  14  15  16  17 │ 18  19  20  21  22  23 │ │ LM5 LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 24  25  26  27  28  29 │ 30  31  32  33  34  35 │ │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
-  ╰───╮ 36  37  38  39  40 │ 41  42  43  44  45 ╭───╯ ╰───╮ LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4 ╭───╯
-      ╰────────────────────┴────────────────────╯         ╰─────────────────────┴─────────────────────╯     */
+  ╭────────────────────────────┬────────────────────────────╮ ╭─────────────────────────────┬─────────────────────────────╮
+  │  0   1   2   3   4   5     │      6   7   8   9  10  11 │ │ LT5 LT4 LT3 LT2 LT1 LT0     │     RT0 RT1 RT2 RT3 RT4 RT5 │
+  │ 12  13  14  15  16  17     │     18  19  20  21  22  23 │ │ LM5 LM4 LM3 LM2 LM1 LM0     │     RM0 RM1 RM2 RM3 RM4 RM5 │
+  │ 24  25  26  27  28  29  30 │ 31  32  33  34  35  36  37 │ │ LB5 LB4 LB3 LB2 LB1 LB0 LEC │ REC RB0 RB1 RB2 RB3 RB4 RB5 │
+  ╰───╮     38  39  40  41  42 │ 43  44  45  46  47     ╭───╯ ╰───╮     LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4     ╭───╯
+      ╰────────────────────────┴────────────────────────╯         ╰─────────────────────────┴─────────────────────────╯     */
 
 #pragma once
 
@@ -44,22 +44,26 @@
 #define LB4 25
 #define LB5 24
 
-#define RB0 30  // right-bottom row
-#define RB1 31
-#define RB2 32
-#define RB3 33
-#define RB4 34
-#define RB5 35
+#define RB0 32  // right-bottom row
+#define RB1 33
+#define RB2 34
+#define RB3 35
+#define RB4 36
+#define RB5 37
 
-#define LH0 40  // left thumb keys
-#define LH1 39
-#define LH2 38
-#define LH3 37
-#define LH4 36
+#define LH0 42  // left thumb keys
+#define LH1 41
+#define LH2 40
+#define LH3 39
+#define LH4 38
 
-#define RH0 41  // right thumb keys
-#define RH1 42
-#define RH2 43
-#define RH3 44
-#define RH4 45
+#define RH0 43  // right thumb keys
+#define RH1 44
+#define RH2 45
+#define RH3 46
+#define RH4 47
+
+// encoders
+#define LEC 30
+#define REC 31
 
