d6a33de3ff3173da40650e10a41a8861b0d1d1dd
Iosif Pravets <chamaileontas@ya.ru>
2025-05-06 09:07:26 +0000

    added key labels for PNCATEHO macropad

---
+++ include/zmk-helpers/key-labels/pncateho_macropad.h
@@ -0,0 +1,23 @@
+/*                                      PNCATEHO KEY MATRIX / LAYOUT MAPPING
+
+  ╭────────────────────╮ ╭─────────────────────╮
+  │  0   1   2   3     │ │ LT3 LT2 LT1 LT0     │
+  │  4   5   6   7     │ │ LB3 LB2 LB1 LB0     │
+  ╰──────────╮   8   9 │ ╰───────────╮ LH1 LH0 │
+             ╰─────────╯             ╰─────────╯             */
+
+#pragma once
+
+#define LT0  3  // top row
+#define LT1  2
+#define LT2  1
+#define LT3  0
+
+#define LB0 4  // bottom row
+#define LB1 5
+#define LB2 6
+#define LB3 7
+
+#define LH0 9  // thumb keys
+#define LH1 8
+
